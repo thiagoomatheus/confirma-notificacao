c8254ce5a04e609aa555b06dd9a184afff12a76f
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-02-18 23:38:50 -0300

    feat(toast): adicionado componente Toaster no layout base
    c

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.className} antialiased`}
       >
+        <Toaster />
         {children}
       </body>
     </html>
