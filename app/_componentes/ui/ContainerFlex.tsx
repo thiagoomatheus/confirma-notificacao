cc26d46e439544274be14f3f3e7b869f6589b8d4
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-02-20 22:02:25 -0300

    refactor(container): mudado de lugar e tirado estilos de width e heigth

--- app/_componentes/ui/ContainerFlex.tsx
+++ app/_componentes/ui/ContainerFlex.tsx
@@ -1,13 +1,14 @@
+import React from "react"
 import { twMerge } from "tailwind-merge"
 
 type ContainerProps = {
     children: React.ReactNode
 } & React.HTMLAttributes<HTMLDivElement>
 
-export default function Container({ children, className, ...rest }: ContainerProps) {
+export default function ContainerFlex({ children, className, ...rest }: ContainerProps) {
     return (
         <div
-            className={twMerge("w-full h-1/2 md:w-1/2 md:h-full flex flex-col items-center justify-center", className)}
+            className={twMerge("flex flex-col items-center justify-center flex-1 w-full h-full", className)}
             {...rest}
         >
             {children}
