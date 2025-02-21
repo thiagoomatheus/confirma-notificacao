621b6df3a72c2296ed61e0ce4f1933f2a0943439
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-02-20 22:03:04 -0300

    refactor(login): alterado nome de componente

--- app/(publica)/entrar/page.tsx
+++ app/(publica)/entrar/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import login from "@/public/Login-amico.svg";
 import Link from "next/link";
-import Container from "./_componentes/ui/Container";
+import ContainerFlex from "../../_componentes/ui/ContainerFlex";
 import BotaoLogin from "./_componentes/ui/BotaoLogin";
 import { auth } from "@/app/_lib/auth/auth";
 import { redirect } from "next/navigation";
@@ -14,21 +14,21 @@ export default async function Page() {
 
     return (
         <section className="self-center flex flex-col md:flex-row items-center justify-start caixa">
-            <Container className="dark:bg-secundaria bg-terciaria rounded-2xl gap-2 p-2">
+            <ContainerFlex className="dark:bg-secundaria bg-terciaria rounded-2xl gap-2 p-2">
                 <Image
                     className="w-full h-full"
                     src={login}
                     alt="Foto retirada de https://storyset.com/online"
                 />
-            </Container>
+            </ContainerFlex>
             
-            <Container className="gap-5 md:gap-10">
+            <ContainerFlex className="gap-5 md:gap-10">
                 <h1 className="text-primaria">Conecte-se</h1>
 
                 <BotaoLogin />
 
                 <p className="text-[10px] text-center w-full max-w-60">Por continuar, você concorda com nossos <Link className="text-[10px] underline underline-offset-2 font-bold" href="/termos-de-serviço">Termos de serviços</Link>. Leia nossas <Link className="text-[10px] underline underline-offset-2 font-bold" href="/politicas-de-privacidade">Políticas de privacidade</Link>.</p>
-            </Container>
+            </ContainerFlex>
         </section>
     )
 }
