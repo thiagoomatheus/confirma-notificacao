1dc3fdc62facf6e042a0cac1fbbd060869f04272
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-02-24 23:51:24 -0300

    refactor(uselogin): adicionado bloco try catch

--- app/(publica)/entrar/_hooks/useLogin.tsx
+++ app/(publica)/entrar/_hooks/useLogin.tsx
@@ -8,11 +8,17 @@ export default function useLogin() {
     const acaoLogin = async () => {
         const toastId = toast.loading("Entrando...")
 
-        const result = await signIn("google", { redirectTo: "/dashboard" })
-
-        if (result?.error) return toast.error(result.error, { id: toastId })
-            
-        return toast.success("Prossiga com o login", { id: toastId })
+        try {
+            const result = await signIn('google', { redirectTo: '/dashboard' });
+            if (result?.error) {
+                toast.error(result.error, { id: toastId });
+            } else {
+                toast.success('Prossiga com o login', { id: toastId });
+            }
+        } catch (error) {
+            toast.error('Erro ao fazer login', { id: toastId });
+            console.error("Erro no login:", error); // Log do erro para debug
+        }
     }
 
     return { acaoLogin }
