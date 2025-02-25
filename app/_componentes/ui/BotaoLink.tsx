66ac62df7dce1d41b690d7bac354c745d6b1f983
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-02-25 17:16:08 -0300

    refactor(botao): alterado estilo de botoes

--- app/_componentes/ui/BotaoLink.tsx
+++ app/_componentes/ui/BotaoLink.tsx
@@ -10,7 +10,7 @@ export default function BotaoLink( {children, className, ...rest}: BotaoProps ) 
     return (
         <Link
             href={rest.href || "#"}
-            className={twMerge("bg-primaria text-texto px-4 py-2 rounded-full font-bold flex items-center justify-center gap-2 outline-1 outline outline-primaria shadow-md duration-200 hover:text-white hover:bg-secundaria hover:underline hover:underline-offset-4 hover:outline-4 focus:outline-4 focus:outline-secundaria", className)}
+            className={twMerge("bg-terciaria dark:bg-primaria text-white px-4 py-2 rounded-full font-bold flex items-center justify-center gap-2 shadow-md duration-200 hover:text-white hover:bg-secundaria dark:hover:bg-terciaria hover:underline hover:underline-offset-4 focus:outline-offset-4 focus:outline-primaria", className)}
             {...rest}
         >
             {children}
