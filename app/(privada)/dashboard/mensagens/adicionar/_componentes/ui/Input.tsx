f30d50e9b7cfb1cffd64503d48d5f1379e67a4ea
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-03-27 22:38:45 -0300

    feat(input): criado componente de input

---
+++ app/(privada)/dashboard/mensagens/adicionar/_componentes/ui/Input.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { FieldError } from "react-hook-form";
+import { twMerge } from "tailwind-merge";
+
+type InputProps = {
+    erro: FieldError | undefined
+} & React.InputHTMLAttributes<HTMLInputElement>
+
+export default function Input( { erro, className, ...rest }: InputProps ) {
+    return (
+        <>
+            <input
+                className={twMerge("max-w-xl p-2", className)}
+                type={rest.type || "text"}
+                aria-invalid={erro ? "true" : "false"}
+                {...rest}
+            />
+            {erro?.message &&
+                <p
+                    className="text-red-600"
+                    role="alert"
+                >
+                    {erro.message}
+                </p>
+            }
+        </>
+    )
+}
