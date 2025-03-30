39061c9d1afa709d6e2e9cc5d1bffe6353033d0e
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-03-30 17:28:43 -0300

    refactor(schema): incluido data minima na validação dos dados

--- app/(privada)/dashboard/mensagens/[id]/notificacoes/_hooks/useNotificacoes.tsx
+++ app/(privada)/dashboard/mensagens/[id]/notificacoes/_hooks/useNotificacoes.tsx
@@ -14,11 +14,11 @@ const notificacaoInicial: Partial<Notificacao> = {
 const schema = z.object({
     idMensagem: z.string(),
     nome: z.optional(z.string().min(1, "O nome da saudação precisa ser preenchido.").max(100, "O nome da saudação precisa ter no máximo 100 caracteres.")),
-    data: z.optional(z.date()),
+    data: z.optional(z.date().min(new Date(), "A data do lembrete precisa ser futura.")),
     hora: z.optional(z.string().time("HH:mm")),
     local: z.optional(z.string().min(1, "O local do lembrete precisa ser preenchido.").max(100, "O local do lembrete precisa ter no máximo 100 caracteres.")),
     obsTexto: z.optional(z.string().min(1, "A observação do lembrete precisa ser preenchida.").max(500, "A observação do lembrete precisa ter no máximo 500 caracteres.")),
-    dataEnvio: z.optional(z.date()),
+    dataEnvio: z.optional(z.date().min(new Date(), "A data de envio do lembrete precisa ser futura.")),
     horaEnvio: z.optional(z.string().time("HH:mm")),
     telefone: z.optional(z.string().regex(/^([14689][0-9]|2[12478]|3([1-5]|[7-8])|5([13-5])|7[193-7])9[0-9]{8}$/, "O telefone precisa ser preenchido.")),
     status: z.optional(z.boolean())
