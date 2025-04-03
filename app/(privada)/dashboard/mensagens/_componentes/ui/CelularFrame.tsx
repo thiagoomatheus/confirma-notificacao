f42ced2714155969e68098d6c9dae5c765d69f7d
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-04-03 16:10:17 -0300

    refactor(celularframe): adicionado informações da notificação

--- app/(privada)/dashboard/mensagens/_componentes/ui/CelularFrame.tsx
+++ app/(privada)/dashboard/mensagens/_componentes/ui/CelularFrame.tsx
@@ -1,16 +1,17 @@
 "use client"
 
 import React from "react"
-import { Mensagem } from "@prisma/client"
+import { Mensagem, Notificacao } from "@prisma/client"
 import { FaList } from "react-icons/fa"
-import { IoSend } from "react-icons/io5"
+import { IoSend, IoArrowBack } from "react-icons/io5"
 import { MdOutlineClose } from "react-icons/md";
 
 type CelularFrameProps = {
     mensagem: Partial<Mensagem>
+    notificacao?: Partial<Notificacao>
 }
 
-export default function CelularFrame( { mensagem }: CelularFrameProps ) {
+export default function CelularFrame( { mensagem, notificacao }: CelularFrameProps ) {
 
     const [mostrarOpcoes, setMostrarOpcoes] = React.useState<boolean>(false)
 
@@ -35,46 +36,70 @@ export default function CelularFrame( { mensagem }: CelularFrameProps ) {
                 <p className="h-[80%] md:h-[60%] flex items-center">{notificacao?.telefone || "Telefone do usuário"}</p>
             </div>
             <div className="celular-conteudo">
-                <div className="flex flex-row pl-5 md:pl-7 pr-2 justify-end">
-                    <div className="celular-mensagem">
-                        {dadosMensagem?.saudacao && <p className="font-bold">{dadosMensagem.saudacao}</p>}
-                        <p>{dadosMensagem?.corpo}</p>
-                        <p>Data: xx/xx/xxxx</p>
-                        <p>Hora: xx:xx</p>
-                        {dadosMensagem.lembreteLocal && <p>Local: {dadosMensagem.localFixo ? dadosMensagem.localFixo : "..... ..... ... ....."}</p>}
-                        {dadosMensagem.obs && <p>Obs: {dadosMensagem.obsFixo ? dadosMensagem.obsFixo : "..... ..... ... ....."}</p>}
-                        <button disabled={!mensagem.chamadaParaAcao} className="flex justify-center items-center gap-2 text-primaria pt-5" onClick={() => setMostrarOpcoes(true)}>
-                            <FaList className="text-lg" />
-                            <p className="font-bold">{dadosMensagem.chamadaParaAcao}</p>
-                        </button>
-                    </div>
-                    <span className="celular-mensagem-detalhe" />
-                </div>
-                {!mostrarOpcoes && 
-                <div className="celular-rodape">
-                    <div className="bg-gray-100 dark:bg-gray-600 w-10/12 h-10 rounded-full" />
-                    <div className="bg-primaria p-3 rounded-full flex items-center justify-center">
-                        <IoSend className="text-lg text-bg" />
-                    </div>
-                </div>
-            }
-            {mensagem.opcoes && mostrarOpcoes && 
-                <div className="celular-opcoes">
-                    <header className="flex items-center border-b border-gray-100 dark:border-gray-600 py-2">
-                        <MdOutlineClose className="text-2xl text-gray-600 absolute cursor-pointer" onClick={() => setMostrarOpcoes(false)} />
-                        {mensagem?.chamadaParaAcao && <p className="font-bold m-auto">{mensagem.chamadaParaAcao}</p>}
-                    </header>
-                    {JSON.parse(mensagem.opcoes as string).map((opcao: string, index: number) => (
-                        <div key={index} className="flex flex-row justify-between items-center">
-                            <p>{opcao}</p>
-                            <input type="radio" disabled />
+                <div className="flex flex-col gap-5">
+                    
+                    <div className="flex flex-row pl-5 md:pl-7 pr-2 justify-end">
+                        <div className="celular-mensagem">
+                            {dadosMensagem.saudacao &&
+                                <p className="font-bold">
+                                    {dadosMensagem.saudacao && mensagem.saudacaoNome && notificacao?.nome ? dadosMensagem.saudacao.replace("{nome}", notificacao?.nome) : dadosMensagem.saudacao}
+                                </p>
+                            }
+                            <p>{dadosMensagem?.corpo}</p>
+                            <p>Data: {notificacao?.data?.toLocaleString("pt-BR").split(",")[0] || "xx/xx/xxxx"}</p>
+                            <p>Hora: {notificacao?.hora?.slice(0, 5) || "xx:xx"}</p>
+                            {dadosMensagem.lembreteLocal &&
+                                <p>
+                                    Local: {(dadosMensagem.localFixo || notificacao?.local) || "..... ..... ... ....."}
+                                </p>
+                            }
+                            {dadosMensagem.obs &&
+                                <p>
+                                    Obs: {(dadosMensagem.obsFixo || notificacao?.obsTexto) || "..... ..... ... ....."}
+                                </p>
+                            }
+                            <p className="text-gray-400 text-xs md:text-sm self-end">{notificacao?.horaEnvio || "xx:xx"}</p>
+                            <button
+                                disabled={!mensagem.chamadaParaAcao}
+                                className="flex justify-center items-center gap-2 text-primaria pt-5"
+                                onClick={() => setMostrarOpcoes(true)}
+                            >
+                                <FaList className="text-lg" />
+                                <p className="font-bold">
+                                    {dadosMensagem.chamadaParaAcao}
+                                </p>
+                            </button>
                         </div>
-                    ))}
-                    <footer className="py-3">
-                        <p className="text-sm text-center">Toque para selecionar um item.</p>
-                    </footer>
+                        <span className="celular-mensagem-detalhe" />
+                    </div>
                 </div>
-            }
+
+                {!mostrarOpcoes && 
+                    <div className="celular-rodape">
+                        <div className="bg-gray-100 dark:bg-gray-600 w-10/12 h-10 rounded-full" />
+                        <div className="bg-primaria p-3 rounded-full flex items-center justify-center">
+                            <IoSend className="text-lg text-bg" />
+                        </div>
+                    </div>
+                }
+
+                {mensagem.opcoes && mostrarOpcoes && 
+                    <div className="celular-opcoes">
+                        <header className="flex items-center border-b border-gray-100 dark:border-gray-600 py-2">
+                            <MdOutlineClose className="text-2xl text-gray-600 absolute cursor-pointer" onClick={() => setMostrarOpcoes(false)} />
+                            {mensagem?.chamadaParaAcao && <p className="font-bold m-auto">{mensagem.chamadaParaAcao}</p>}
+                        </header>
+                        {JSON.parse(mensagem.opcoes as string).map((opcao: string, index: number) => (
+                            <div key={index} className="flex flex-row justify-between items-center">
+                                <p>{opcao}</p>
+                                <input type="radio" disabled />
+                            </div>
+                        ))}
+                        <footer className="py-3">
+                            <p className="text-sm text-center">Toque para selecionar um item.</p>
+                        </footer>
+                    </div>
+                }
             </div>
         </div>
     )
