ded3cb07336fc5a12f9ca7137e7fb828c9efd267
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-04-03 16:19:23 -0300

    refactor(usenotificacoes): trazido schema para dentro do escopo da função e aperfeiçoado validações

--- app/(privada)/dashboard/mensagens/[id]/notificacoes/_hooks/useNotificacoes.tsx
+++ app/(privada)/dashboard/mensagens/[id]/notificacoes/_hooks/useNotificacoes.tsx
@@ -1,44 +1,76 @@
-import React from "react"
-import { Notificacao } from "@prisma/client";
+"use client"
+
 import { useState } from "react";
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import dayjs from 'dayjs'
 
 const notificacaoInicial: Partial<Notificacao> = {
-    nome: "",
+    nome: null,
     telefone: "",
     status: false
 }
 
-const schema = z.object({
-    idMensagem: z.string(),
-    nome: z.optional(z.string().min(1, "O nome da saudação precisa ser preenchido.").max(100, "O nome da saudação precisa ter no máximo 100 caracteres.")),
-    data: z.optional(z.date().min(new Date(), "A data do lembrete precisa ser futura.")),
-    hora: z.optional(z.string().time("HH:mm")),
-    local: z.optional(z.string().min(1, "O local do lembrete precisa ser preenchido.").max(100, "O local do lembrete precisa ter no máximo 100 caracteres.")),
-    obsTexto: z.optional(z.string().min(1, "A observação do lembrete precisa ser preenchida.").max(500, "A observação do lembrete precisa ter no máximo 500 caracteres.")),
-    dataEnvio: z.optional(z.date().min(new Date(), "A data de envio do lembrete precisa ser futura.")),
-    horaEnvio: z.optional(z.string().time("HH:mm")),
-    telefone: z.optional(z.string().regex(/^([14689][0-9]|2[12478]|3([1-5]|[7-8])|5([13-5])|7[193-7])9[0-9]{8}$/, "O telefone precisa ser preenchido.")),
-    status: z.optional(z.boolean())
-})
+export default function useNotificacoes(idMensagem?: Mensagem["id"], notificacaoExistente?: Notificacao) {
+    
+    const [notificacao, setNotificacao] = useState<Partial<Notificacao> | undefined>(notificacaoExistente || undefined)
+    
+    const totalDeCampos = 3
+    const [camposPreenchidos, setCamposPreenchidos] = useState<number>(0)
 
-export type Inputs = z.infer<typeof schema>
+    const schema = z.object({
+        nome: z.optional(
+            z.string()
+            .min(1, "O nome da saudação precisa ser preenchido.")
+            .max(100, "O nome da saudação precisa ter no máximo 100 caracteres.")
+        ),
+        dataEHora: z.optional(
+            z.string()
+            .refine(data => !data || dayjs(data).isValid(), "Data inválida")
+            .refine(data => dayjs().isBefore(data), "Menor que momento atual.")
+        ),
+        local: z.optional(
+            z.string()
+            .min(1, "O local do lembrete precisa ser preenchido.")
+            .max(100, "O local do lembrete precisa ter no máximo 100 caracteres.")
+        ),
+        obsTexto: z.optional(
+            z.string()
+            .min(1, "A observação do lembrete precisa ser preenchida.")
+            .max(500, "A observação do lembrete precisa ter no máximo 500 caracteres.")
+        ),
+        dataEHoraEnvio: z.optional(
+            z.string()
+            .refine(data => !data || dayjs(data).isValid(), "Data inválida")
+            .refine(data => dayjs().isBefore(data), "Menor que momento atual.")
+            .refine(data => !data || dayjs(data).isAfter(dayjs(notificacao?.data || '')), "Antes do compromisso.")
+        ),
+        telefone: z.optional(
+            z.string()
+            .min(10, "O telefone precisa ser preenchido.")
+            .refine(telefoneBruto => {
+                const regexTelefone: RegExp = /^([14689][0-9]|2[12478]|3([1-5]|[7-8])|5([13-5])|7[193-7])9[0-9]{8}$/
+                const telefone = telefoneBruto.startsWith("+") ? telefoneBruto.slice(3).trim().replace(/[^0-9]/g, "") : telefoneBruto.trim().replace(/[^0-9]/g, "")
+                return regexTelefone.test(telefone)
+            }, "Telefone inválido.")
+        ),
+    })
+    
+    type Inputs = z.infer<typeof schema>
 
-export default function useNotificacoes(notificacaoExistente?: Notificacao) {
-
+    const stringData = `${notificacaoExistente?.dataEnvio.toLocaleString("pt-BR").split(",")[0]} ${notificacaoExistente?.horaEnvio}`
+    
     const { register, handleSubmit, formState: { errors } } = useForm<Inputs>({
         resolver: zodResolver(schema),
         defaultValues: notificacaoExistente && {
             ...notificacaoExistente,
             nome: notificacaoExistente?.nome || "",
-            data: notificacaoExistente?.data || new Date(),
-            hora: notificacaoExistente?.hora || "",
+            dataEHora: dayjs(stringData).format("YYYY-MM-DD HH:mm"),
             local: notificacaoExistente?.local || "",
             obsTexto: notificacaoExistente?.obsTexto || "",
             telefone: notificacaoExistente?.telefone || "",
-            status: notificacaoExistente?.status || false
+            dataEHoraEnvio: undefined,
         }
     });
 
