2bea970863f6b347182efae71633ce5e5d50c68b
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-04-05 23:20:55 -0300

    refactor(layout): ajustado layout para inclusão de menu

--- app/(privada)/layout.tsx
+++ app/(privada)/layout.tsx
@@ -1,16 +1,20 @@
 import { estaLogado } from "../utils/estaLogado";
 import BarraNavegacao from "./dashboard/_componentes/ui/BarraNavegacao";
+import Menu from "./dashboard/_componentes/ui/Menu";
 
 export default async function LayoutPrivate( { children }: { children: React.ReactNode } ) {
 
     await estaLogado()
 
     return (
-        <main className="min-h-screen flex flex-col">
-            <BarraNavegacao />
-            <section className="p-4 w-full max-w-7xl flex flex-col self-center gap-5 md:gap-10">
-                {children}
-            </section>
-        </main>
+        <div className="min-h-screen flex flex-col lg:flex-row items-start">
+            <Menu />
+            <div className="flex flex-col w-full">
+                <BarraNavegacao />
+                <main className="p-4 w-full max-w-7xl flex flex-col self-center gap-5 md:gap-10">
+                    {children}
+                </main>
+            </div>
+        </div>
     )
 }
