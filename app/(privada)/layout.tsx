52e66f5a617584d26927aee2ca3fc337abc22708
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-04-15 15:29:56 -0300

    refactor(layout): ajustado layout para comportamento do compoente menu

--- app/(privada)/layout.tsx
+++ app/(privada)/layout.tsx
@@ -1,20 +1,16 @@
 import { estaLogado } from "../utils/estaLogado";
 import BarraNavegacao from "./dashboard/_componentes/ui/BarraNavegacao";
-import Menu from "./dashboard/_componentes/ui/Menu";
 
 export default async function LayoutPrivate( { children }: { children: React.ReactNode } ) {
 
     await estaLogado()
 
     return (
-        <div className="min-h-screen flex flex-col lg:flex-row items-start">
-            <Menu />
-            <div className="flex flex-col w-full">
-                <BarraNavegacao />
-                <main className="p-4 w-full max-w-7xl flex flex-col self-center gap-5 md:gap-10">
-                    {children}
-                </main>
-            </div>
+        <div className="flex flex-col w-full">
+            <BarraNavegacao />
+            <main className="p-4 w-full max-w-7xl flex flex-col self-center gap-5 md:gap-10">
+                {children}
+            </main>
         </div>
     )
 }
