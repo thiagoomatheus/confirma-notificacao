83c181aabfcd3bea1b8ded7740ba54f74ea5d3a0
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-04-15 15:29:10 -0300

    feat(menu): refatorado menu para ser visivel apenas em rotas privadas

--- app/(privada)/dashboard/_componentes/ui/Menu.tsx
+++ app/(privada)/dashboard/_componentes/ui/Menu.tsx
@@ -3,74 +3,100 @@
 import React from "react"
 import { TbLayoutSidebarLeftExpand, TbLayoutSidebarLeftCollapseFilled, TbMessageSearch, TbMessagePlus } from "react-icons/tb";
 import { PiUserCircle } from "react-icons/pi";
-import Link from "next/link";
 import { FaWhatsapp } from "react-icons/fa";
 import { MdOutlineDashboard } from "react-icons/md";
+import { IoMdLogOut } from "react-icons/io";
+import LinkMenu from "./LinkMenu";
+import { usePathname } from "next/navigation";
+import { signOut } from "next-auth/react";
+import { twMerge } from "tailwind-merge";
 
-export default function Menu() {
+type MenuProps = React.HTMLAttributes<HTMLDivElement>
 
+export default function Menu( { className, ...rest }: MenuProps ) {
+
+    const minhaRota = usePathname()
     const [menuAberto, setMenuAberto] = React.useState(false)
 
+    const rotasPublicas = ["/", "/login"]
+
     return (
-        <div className={`bg-gray-100 dark:bg-secundaria shadow-lg flex flex-col gap-10 p-5 md:relative top-0 left-0 lg:h-screen z-50 ${menuAberto ? "w-full lg:w-80 fixed h-screen aberto" : "fechado"} menu`}>
+        <div
+            className={twMerge(`bg-gray-100 dark:bg-secundaria flex flex-col gap-10 p-5 lg:sticky top-0 left-0 lg:h-screen z-10 ${menuAberto ? "w-full sm:w-80 h-screen fixed aberto" : "fechado"} menu ${rotasPublicas.includes(minhaRota) ? "hidden" : ""}`, className?.includes("flex") && rotasPublicas.includes(minhaRota) ? "hidden" : className)}
+            {...rest}
+        >
             
             {!menuAberto &&
                 <TbLayoutSidebarLeftExpand
-                    className="text-3xl cursor-pointer"
+                    className="text-3xl cursor-pointer hover:text-primaria duration-500"
                     onClick={() => setMenuAberto(!menuAberto)}
                 />
             }
 
             {menuAberto &&
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-row justify-between items-center gap-2">
                     <TbLayoutSidebarLeftCollapseFilled
-                        className="text-3xl cursor-pointer"
+                        className="text-3xl cursor-pointer hover:text-primaria duration-500"
                         onClick={() => setMenuAberto(!menuAberto)}
                     />
+                    <h1 className="text-2xl font-bold lg:hidden">{process.env.NEXT_PUBLIC_NOME_APP}</h1>
                 </div>
             }
 
-            <nav className={`${menuAberto ? "flex" : "hidden"} lg:flex flex-col items-start gap-5`}>
+            <nav className={`${menuAberto ? "flex" : "hidden"} lg:flex flex-col items-start gap-5 h-full`}>
 
-                <Link
-                    className="flex flex-row gap-2 justify-start items-center"
+                <LinkMenu
                     href="/dashboard"
-                >
-                    <MdOutlineDashboard className="text-3xl" />
-                    {menuAberto && <p>Dashboard</p>}
-                </Link>
+                    estado={menuAberto}
+                    icone={MdOutlineDashboard}
+                    texto="Dashboard"
+                    minhaRota={minhaRota}
+                />
 
-                <Link
-                    className="flex flex-row gap-2 justify-start items-center"
+                <LinkMenu
                     href="/dashboard/mensagens"
-                >
-                    <TbMessageSearch className="text-3xl" />
-                    {menuAberto && <p>Mensagens</p>}
-                </Link>
+                    estado={menuAberto}
+                    icone={TbMessageSearch}
+                    texto="Mensagens"
+                    minhaRota={minhaRota}
+                />
 
-                <Link
-                    className="flex flex-row gap-2 justify-start items-center"
+                <LinkMenu
                     href="/dashboard/mensagens/adicionar"
-                >
-                    <TbMessagePlus className="text-3xl" />
-                    {menuAberto && <p className="lg:w-40">Nova mensagem</p>}
-                </Link>
-                
-                <Link
-                    className="flex flex-row gap-2 justify-start items-center"
-                    href="/perfil/whatsapp"
-                >
-                    <FaWhatsapp className="text-3xl" />
-                    {menuAberto && <p>WhatsApp</p>}
-                </Link>
+                    estado={menuAberto}
+                    icone={TbMessagePlus}
+                    texto="Nova mensagem"
+                    minhaRota={minhaRota}
+                />
 
-                <Link
-                    className="flex flex-row gap-2 justify-start items-center absolute bottom-5"
-                    href="/perfil"
-                >
-                    <PiUserCircle className="text-3xl" />
-                    {menuAberto && <p>Perfil</p>}
-                </Link>
+                <LinkMenu
+                    href="/perfil/whatsapp"
+                    estado={menuAberto}
+                    icone={FaWhatsapp}
+                    texto="Meu WhatsApp"
+                    minhaRota={minhaRota}
+                />
+                
+                <div className="flex flex-col gap-5 absolute bottom-5">
+                    <LinkMenu
+                        href="/perfil"
+                        estado={menuAberto}
+                        icone={PiUserCircle}
+                        texto="Perfil"
+                        minhaRota={minhaRota}
+                    />
+                    
+                    <button
+                        className="flex flex-row gap-2 justify-start items-center hover:text-primaria duration-500"
+                        onClick={() => signOut()}
+                    >
+                        <IoMdLogOut
+                            className="text-3xl"
+                        />
+                        {menuAberto && <p>Sair</p>}
+                    </button>
+                </div>
+
             </nav>
         </div>
     )
