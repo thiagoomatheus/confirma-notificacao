d984ca4c4214d8a773d353de6c032a7a7b51aa1e
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-04-16 00:48:42 -0300

    feat(nextconfig): adicionado rota para uso de url em componente Image

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone"
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
